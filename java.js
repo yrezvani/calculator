5ba3ac5023ee3fe57b4ac8c9e9e197da66521341
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-09-26 16:57:18 +0100

    Add buttons event listener and get screen output each button get stored in firstNum and is outputted on screen.

--- java.js
+++ java.js
@@ -26,4 +26,47 @@ const operate = function (operator) {
     if (operator === '/') return divide (Number(firstNum), Number(secondNum));
 }
 
+const nums = document.querySelectorAll('.numbers');
+const scrnTxt = document.querySelector('#screen-text');
 
+
+nums.forEach(function (i) {
+    i.addEventListener('click', (e) => {
+        if (firstNum == null) {
+            firstNum = e.target.textContent;
+            scrnTxt.textContent = firstNum;
+        } else {
+            firstNum = firstNum + e.target.textContent;
+            scrnTxt.textContent = firstNum;
+        }
+    });
+});
+
+
+
+
+//     addEventListener ("click", (e) => {
+//     console.log(e.target);
+// })
+    // if (firstNum == null) {
+    //     firstNum = e.target.textContent;
+    // } else {
+    //     firstNum = firstNum + e.target.textContent;
+    // }
+// })
+
+// const button7 = document.querySelector('#seven');
+// seven.addEventListener ('click', () => {
+//     if (firstNum == null) {
+//         firstNum = button7.textContent;
+//     } else {
+//         firstNum = firstNum + '7'
+//     }
+// })
+
+    //     if (firstNum = null) {
+//         firstNum = buttons.textContent;
+//     } else {
+//         firstNum = firstNum + buttons.textContent;
+//     }
+// })
